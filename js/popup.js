17a5f106b2c89466546559e8400bab036eb38600
greg <gregoryc@yeah.net>
2017-06-05 19:29:40 +0800

    add css stylesheet

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,7 @@
 chrome.runtime.sendMessage({
     aim: 'requestData'
 },function (response) {
-    $("#recordTable").append(response.data);
+    $("#recordTable > tbody").append(response.data);
+    $('#recordTable tbody tr:nth-child(odd)').css("background-color","lightgrey").next().css("background-color","lightblue");
+    $('td.detail a.record-icon.icon-detail.icon-detail-trigger').html('<img src="images/icon.png" alt="detail">').attr("target","_blank");
 });
